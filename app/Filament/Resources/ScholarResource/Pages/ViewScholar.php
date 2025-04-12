4bb6180c142aa0e0116a065c8baaecac521f44aa
Yuuki <yuuki@example.com>
2025-04-12 10:49:37 +0800

    wip

---
+++ app/Filament/Resources/ScholarResource/Pages/ViewScholar.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Resources\ScholarResource\Pages;
+
+use App\Filament\Resources\ScholarResource;
+use Filament\Actions;
+use Filament\Resources\Pages\ViewRecord;
+
+class ViewScholar extends ViewRecord
+{
+    protected static string $resource = ScholarResource::class;
+}
